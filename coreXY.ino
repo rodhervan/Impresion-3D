7f536e651590d47c2ba500d13411eebe89ef07c9
Rodrigo Hernández Vanegas <64353819+rodhervan@users.noreply.github.com>
2023-10-21 23:31:52 -0500

    Add files via upload

--- coreXY.ino
+++ coreXY.ino
@@ -7,15 +7,22 @@
 #define STEPPER2_DIR_PIN 7
 #define STEPPER2_STEP_PIN 6
 
-int posx = 0;
-int posy = 0;
+struct Coordinates {
+  int posx;
+  int posy;
+};
 
 // Define some steppers and the pins the will use
 AccelStepper stepper1(AccelStepper::DRIVER, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN);
 AccelStepper stepper2(AccelStepper::DRIVER, STEPPER2_STEP_PIN, STEPPER2_DIR_PIN);
 
+Coordinates currentCoords;
+
 void setup() {
   Serial.begin(9600);
+  currentCoords.posx = 0;
+  currentCoords.posy = 0;
+
   stepper1.setMaxSpeed(200.0);
   stepper1.setAcceleration(200.0);
   stepper1.moveTo(100);
@@ -23,32 +30,52 @@ void setup() {
   stepper2.setMaxSpeed(100.0);
   stepper2.setAcceleration(100.0);
   stepper2.moveTo(100);
+
 }
 
 void loop() {
-  leerSerial();
-  // Change direction at the limits
+  // Revisa si hay nuevas coordenadas
+  Coordinates newCoords = leerSerial();
+
+  // actualiza si hay disponibles
+  if (newCoords.posx != -1) {
+    currentCoords = newCoords;
+  }
+
+  // Nuevas coordenadas a usar
+  Serial.print("posx = ");
+  Serial.println(currentCoords.posx);
+  Serial.print("posy = ");
+  Serial.println(currentCoords.posy);
+
+  // Cambiar movimiento de los motores
   if (stepper1.distanceToGo() == 0)
-    stepper1.moveTo(posx);
+    stepper1.moveTo(currentCoords.posx);
+    
   if (stepper2.distanceToGo() == 0)
-    stepper2.moveTo(posy);
+    stepper2.moveTo(currentCoords.posy);
   stepper1.run();
   stepper2.run();
 }
 
 // se deben ingresar las coordenadas en el formato x=#,y=# ej: x=100,y=200
-void leerSerial() {
+Coordinates leerSerial() {
+  Coordinates result;
+  result.posx = -1; // valor para indicar que no hay coordenadas nuevas
+  result.posy = -1;
+
   if (Serial.available() > 0) {
     String inputString = Serial.readStringUntil('\n');
     if (inputString.startsWith("x=")) {
       int y_sub = inputString.indexOf(",");
-      int posx = inputString.substring(2, y_sub).toInt();
-      int posy = inputString.substring(y_sub+3).toInt();
+      result.posx = inputString.substring(2, y_sub).toInt();
+      result.posy = inputString.substring(y_sub + 3).toInt();
       Serial.print("El valor ingresado fue: x = ");
-      Serial.print(posx);
+      Serial.print(result.posx);
       Serial.print(", y = ");
-      Serial.println(posy);
+      Serial.println(result.posy);
     }
   }
+  return result;
 }
 
