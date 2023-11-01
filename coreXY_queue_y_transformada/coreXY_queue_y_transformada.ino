aa6edc9fadb3cd7f7158615fb197c00bc623e2cb
Rodrigo Hernández Vanegas <64353819+rodhervan@users.noreply.github.com>
2023-11-01 02:51:01 -0500

    Add files via upload

--- coreXY_queue_y_transformada/coreXY_queue_y_transformada.ino
+++ coreXY_queue_y_transformada/coreXY_queue_y_transformada.ino
@@ -13,10 +13,10 @@ struct Coordinates {
 };
 
 
-float maxX = 220;
-float maxY = 200;
-float minX = 0;
-float minY = 0;
+float maxX = 10000;
+float maxY = 10000;
+float minX = -10000;
+float minY = -10000;
 
 // Ajustar dependiendo de cuanto se mueva cada motor
 double ScaleFactorA = 1;
@@ -24,6 +24,13 @@ double ScaleFactorB = 1;
 
 // Maximo de coordenadas almacenadas en un tiempo dado
 const int maxQueueSize = 36;
+
+// Queue to store the last 5 positions from stepper1
+const int StqueueSize = 100;
+int positionQueue1[StqueueSize];
+int positionQueue2[StqueueSize];
+int queueIndex1 = 0;
+int queueIndex2 = 0;
 
 // Definir los steppers y pines que se van a usar (si no funciona el primer parámetro probar con el número 1)
 AccelStepper stepper1(AccelStepper::DRIVER, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN);
@@ -51,7 +58,35 @@ void setup() {
   stepper2.moveTo(0);
 }
 
+void addToQueue1(int position) {
+  positionQueue1[queueIndex1] = position;
+  queueIndex1 = (queueIndex1 + 1) % StqueueSize;
+}
+// Function to add a position to the queue for stepper2
+void addToQueue2(int position) {
+  positionQueue2[queueIndex2] = position;
+  queueIndex2 = (queueIndex2 + 1) % StqueueSize;
+}
+bool areAllEqual1(int value) {
+  for (int i = 0; i < StqueueSize; i++) {
+    if (positionQueue1[i] != value || value == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+// Function to check if all elements in the queue for stepper2 are the same
+bool areAllEqual2(int value) {
+  for (int i = 0; i < StqueueSize; i++) {
+    if (positionQueue2[i] != value || value == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 void loop() {
+  
   // Revisa si hay nuevas coordenadas
   Coordinates newCoords = leerSerial();
   if (newCoords.posx != -1) {
@@ -59,11 +94,44 @@ void loop() {
       queue[queueSize] = newCoords;
       queueSize++;
       Serial.println(queueSize);
+      updateMotorTargets();
     }
   }
- //Serial.print(stepper1.distanceToGo());
- //Serial.print("  ");
- //Serial.println(stepper2.distanceToGo());
+  int stepper1Position = stepper1.distanceToGo();
+  int stepper2Position = stepper2.distanceToGo();
+  addToQueue1(stepper1Position);
+  addToQueue2(stepper2Position);
+  Serial.print(stepper1.distanceToGo());
+  Serial.print("  ");
+  Serial.println(stepper2.distanceToGo());
+
+  if (areAllEqual1(stepper1Position)) {
+    // Do something when all elements in the queue are the same (excluding zero)
+    // Replace the following line with your desired action.
+    Serial.println("All elements in the queue are the same");
+    stepper1.moveTo(stepper1.currentPosition());
+    stepper2.moveTo(stepper2.currentPosition());
+    stepper1.run();
+    stepper2.run();
+        // Reset all elements in the queue to zero
+    for (int i = 0; i < StqueueSize; i++) {
+      positionQueue1[i] = 0;
+    }
+
+  }
+  if (areAllEqual2(stepper2Position)) {
+    // Do something when all elements in the queue for stepper2 are the same (excluding zero)
+    Serial.println("All elements in the queue for stepper2 are the same");
+    stepper1.moveTo(stepper1.currentPosition());
+    stepper2.moveTo(stepper2.currentPosition());
+    stepper1.run();
+    stepper2.run();
+    
+    // Reset all elements in the queue for stepper2 to zero
+    for (int i = 0; i < StqueueSize; i++) {
+      positionQueue2[i] = 0;
+    }
+  }
 
 
   // Cambiar movimiento de los motores
@@ -121,23 +189,32 @@ Coordinates leerSerial() {
 }
 
 Coordinates transformacion_a_corexy(const Coordinates& current, const Coordinates& previous){
-  Coordinates delta;
-  //// calculo de los Deltas
-  // revisa que este dentro de los limites
-  if ((current.posx > maxX)||(current.posy > maxY)||(current.posx < minX)||(current.posy < minY)){
-    delta.posx = 0;
-    delta.posy = 0;
-    Serial.println("Fuera del rango");
-  }
-  else{
-  delta.posx = current.posx - previous.posx;
-  delta.posy = current.posy - previous.posy;
-  }
+
+  // // revisa que este dentro de los limites
+  // if ((current.posx > maxX)||(current.posy > maxY)||(current.posx < minX)||(current.posy < minY)){
+  //   Serial.println("Fuera del rango");
+  // }
  ////// tranformar deltas en x,y a movimiento de motores en el coreXY
   Coordinates motor;
   // lo transformo a int porque no sé si la función de los steppers recibe input double
-  motor.posx = int(ScaleFactorA*delta.posx + ScaleFactorB*delta.posy);
-  motor.posy = int(ScaleFactorA*delta.posx - ScaleFactorB*delta.posy);
+  //motor.posx = int(ScaleFactorA*(delta.posx + delta.posy));
+  
+  //motor.posy = int(ScaleFactorB*(delta.posx - delta.posy));
+  //stepper1 lado izquierdo del corexy
+  motor.posx = int(-ScaleFactorA*(current.posx + current.posy));
+  //stepper2 lado derecho del corexy
+  motor.posy = int(ScaleFactorB*(current.posx - current.posy));
+
+
 
   return motor;
+}
+
+void updateMotorTargets() {
+  if (queueSize > 0) {
+    Coordinates current = queue[0];
+    Coordinates motor = transformacion_a_corexy(current, currentCoords);
+    stepper1.moveTo(motor.posx);
+    stepper2.moveTo(motor.posy);
+  }
 }
